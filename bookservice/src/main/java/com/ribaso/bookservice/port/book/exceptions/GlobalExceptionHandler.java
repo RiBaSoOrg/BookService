92e6deb31f1801cd751d6e3a02579c61c1a27048
Rico <rico.flemming@student.htw-berlin.de>
2024-06-27 19:00:42 +0200

    doku

--- bookservice/src/main/java/com/ribaso/bookservice/port/book/exceptions/GlobalExceptionHandler.java
+++ bookservice/src/main/java/com/ribaso/bookservice/port/book/exceptions/GlobalExceptionHandler.java
@@ -5,10 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
+/**
+ * Global exception handler for handling exceptions across the whole application.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    /**
+     * Handles BookNotFoundException and returns a 404 Not Found response.
+     *
+     * @param ex the exception that was thrown
+     * @param request the current request
+     * @return a ResponseEntity containing the exception message and a 404 Not Found status
+     */
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<?> handleBookNotFoundException(BookNotFoundException ex, WebRequest request) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
