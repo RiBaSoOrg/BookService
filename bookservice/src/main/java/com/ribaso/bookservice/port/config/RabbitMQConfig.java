524cb445f8d3eddf4b2542b4af7ba7a7d6c9c1c3
Rico <rico.flemming@student.htw-berlin.de>
2024-07-03 22:55:17 +0200

    ohne json format probieren

--- bookservice/src/main/java/com/ribaso/bookservice/port/config/RabbitMQConfig.java
+++ bookservice/src/main/java/com/ribaso/bookservice/port/config/RabbitMQConfig.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -29,24 +30,5 @@ public class RabbitMQConfig {
     public Binding binding(Queue bookQueue, DirectExchange bookExchange) {
         return BindingBuilder.bind(bookQueue).to(bookExchange).with("bookRoutingKey");
     }
-
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter());
-        return factory;
-    }
+  
 }
