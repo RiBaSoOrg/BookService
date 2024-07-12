c92d5dbaceafb91cca6a717e3f7344c04a9da9b3
Rico <rico.flemming@student.htw-berlin.de>
2024-07-12 23:33:31 +0200

    RabbitMQ bugfix v.06.1

--- bookservice/src/main/java/com/ribaso/bookservice/port/config/RabbitMQConfig.java
+++ bookservice/src/main/java/com/ribaso/bookservice/port/config/RabbitMQConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -14,8 +15,8 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     @Bean
-    public DirectExchange bookExchange() {
-        return new DirectExchange("bookExchange");
+    public TopicExchange bookExchange() {
+        return new TopicExchange("bookExchange");
     }
 
     @Bean
