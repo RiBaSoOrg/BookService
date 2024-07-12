41db1ad15167b8b2f1072db45641fb3a5f200b79
Rico <rico.flemming@student.htw-berlin.de>
2024-07-12 09:15:09 +0200

    erster swagger versuch

---
+++ bookservice/src/main/java/com/ribaso/bookservice/port/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package com.ribaso.bookservice.port.config;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("spring")
+                .packagesToScan("com.ribaso.checkoutservice.port.user.controller")
+                .build();
+    }
+}
