4a4cf3f68194257e0dc2b1ea11061c27ccaecc0c
Rico <rico.flemming@student.htw-berlin.de>
2024-07-14 10:25:09 +0200

    RabbitMQ  bugfix v.092(correlationId auch in der Rückantwort v2)

--- bookservice/src/main/java/com/ribaso/bookservice/port/user/consumer/BookConsumer.java
+++ bookservice/src/main/java/com/ribaso/bookservice/port/user/consumer/BookConsumer.java
@@ -1,20 +1,17 @@
 package com.ribaso.bookservice.port.user.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ribaso.bookservice.core.domain.model.Book;
 import com.ribaso.bookservice.core.domain.service.interfaces.BookService;
 import com.ribaso.bookservice.port.exceptions.BookNotFoundException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,19 +30,26 @@ public class BookConsumer {
     }
 
     @RabbitListener(queues = "bookQueue")
-public void getBookDetails(String bookId, @Header("correlationId") String correlationId) throws BookNotFoundException {
-    Book book = bookService.getBook(bookId);
-    System.out.println(bookId);
-    System.out.println("Received book: " + book.getTitle());
-    try {
-        MessagePostProcessor messagePostProcessor = message -> {
-            message.getMessageProperties().setCorrelationId(correlationId);
-            return message;
-        };
-        rabbitTemplate.convertAndSend("bookExchange", "bookResponseRoutingKey", book, messagePostProcessor);
-        log.info("Book sent: " + book.getTitle() + " with correlation ID: " + correlationId);
-    } catch (Exception e) {
-        throw new RuntimeException("Failed to serialize book details", e);
+    public void getBookDetails(String bookId, @Header("correlation_id") String correlationId) throws BookNotFoundException {
+        Book book = bookService.getBook(bookId);
+        log.info("Received book: " + book.getTitle());
+        log.info("Sending book details for ID: {} with correlationId: {}", bookId, correlationId);
+    
+        try {
+            rabbitTemplate.convertAndSend(
+                "bookExchange",
+                "bookResponseRoutingKey",
+                book,
+                message -> {
+                    MessageProperties properties = message.getMessageProperties();
+                    properties.setCorrelationId(correlationId);
+                    return message;
+                }
+            );
+            log.info("Book sent: " + book.getTitle() + " with correlation ID: " + correlationId);
+        } catch (Exception e) {
+            log.error("Failed to send book details", e);
+            throw new RuntimeException("Failed to send book details", e);
+        }
     }
-}
 }
