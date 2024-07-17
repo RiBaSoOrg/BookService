c1c4a57a9fd6faf310a4a44a4cedfb29e24ef658
Rico <rico.flemming@student.htw-berlin.de>
2024-07-17 22:47:09 +0200

    unused imports

--- bookservice/src/main/java/com/ribaso/bookservice/core/domain/service/impl/BookServiceImpl.java
+++ bookservice/src/main/java/com/ribaso/bookservice/core/domain/service/impl/BookServiceImpl.java
@@ -2,7 +2,6 @@ package com.ribaso.bookservice.core.domain.service.impl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.ribaso.bookservice.core.domain.model.Book;
